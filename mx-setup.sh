50eb467fccf92246ec09bcec5f16704ae945ca62
chris c <cchalifo999@gmail.com>
2025-04-16 00:12:57 -0400

    Update mx-setup.sh

--- mx-setup.sh
+++ mx-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 read -p Domain_Name: DOMAINN 
-docker run -it --rm -v $(pwd)/appdata/synapse:/data -e SYNAPSE_SERVER_NAME=mx.${DOMAINN}  -e SYNAPSE_REPORT_STATS=no  -e UID=$(id -g) -e GID=$(id -g) ghcr.io/element-hq/synapse generate
+docker run -it --rm -v $(pwd)/appdata/matrix/synapse:/data -e SYNAPSE_SERVER_NAME=mx.${DOMAINN}  -e SYNAPSE_REPORT_STATS=no  -e UID=$(id -g) -e GID=$(id -g) ghcr.io/element-hq/synapse generate
 ls -la $(pwd)/appdata/synapse
